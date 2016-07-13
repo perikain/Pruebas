114b8c8657cbed1ab48d0bf31c1457ca899a2db5
A.R <perikain@gmail.com>
2016-07-14 00:53:03 +0200

    prueba

--- prueba.py
+++ prueba.py
@@ -2,15 +2,11 @@
 #!-*- coding: utf-8 -*-
 
 
-import socket
+import class_server
+import class_client
 
-host = "192.168.56.1"
-port = 12345
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(1)
 
-while True:
-    conn,addr = s.accept()
-    print addr, "Now connected"
-    conn.send("Thank you for connecting")
+servidor = class_server.Server()
+servidor.open_socket()
+
+
